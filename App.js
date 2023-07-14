40de5ca1ab49dbe9ffd6ab2e45be8c2141f6c5a6
Hussain Mustafa <hussain.mustafa@codingo.onmicrosoft.com>
2023-07-14 17:58:32 +0500

    fresh

--- App.js
+++ App.js
@@ -1,36 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { TouchableOpacity } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
-// import NfcManager, { NfcTech } from 'react-native-nfc-manager';
+
 export default function App() {
-  const readNdef = async () => {
-    // NfcManager.start();
-    // try {
-    //   // register for the NFC tag with NDEF in it
-    //   await NfcManager.requestTechnology(NfcTech.Ndef);
-    //   // the resolved tag object will contain `ndefMessage` property
-    //   const tag = await NfcManager.getTag();
-    //   console.warn('Tag found', tag);
-    // } catch (ex) {
-    //   console.warn('Oops!', ex);
-    // } finally {
-    //   // stop the nfc scanning
-    //   NfcManager.cancelTechnologyRequest();
-    // }
-  }
   return (
-    <View style={styles.wrapper}>
-      <TouchableOpacity onPress={readNdef}>
-        <Text>Scan a Tag</Text>
-      </TouchableOpacity>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  wrapper: {
+  container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#fff',
     alignItems: 'center',
+    justifyContent: 'center',
   },
 });
