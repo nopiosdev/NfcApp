fcd787c4d41f65084724441122463cce16797494
Hussain Mustafa <hussain.mustafa@codingo.onmicrosoft.com>
2023-07-14 17:08:26 +0500

    build configuration

--- App.js
+++ App.js
@@ -1,22 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { TouchableOpacity } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
-import NfcManager, { NfcTech } from 'react-native-nfc-manager';
+// import NfcManager, { NfcTech } from 'react-native-nfc-manager';
 export default function App() {
   const readNdef = async () => {
-    NfcManager.start();
-    try {
-      // register for the NFC tag with NDEF in it
-      await NfcManager.requestTechnology(NfcTech.Ndef);
-      // the resolved tag object will contain `ndefMessage` property
-      const tag = await NfcManager.getTag();
-      console.warn('Tag found', tag);
-    } catch (ex) {
-      console.warn('Oops!', ex);
-    } finally {
-      // stop the nfc scanning
-      NfcManager.cancelTechnologyRequest();
-    }
+    // NfcManager.start();
+    // try {
+    //   // register for the NFC tag with NDEF in it
+    //   await NfcManager.requestTechnology(NfcTech.Ndef);
+    //   // the resolved tag object will contain `ndefMessage` property
+    //   const tag = await NfcManager.getTag();
+    //   console.warn('Tag found', tag);
+    // } catch (ex) {
+    //   console.warn('Oops!', ex);
+    // } finally {
+    //   // stop the nfc scanning
+    //   NfcManager.cancelTechnologyRequest();
+    // }
   }
   return (
     <View style={styles.wrapper}>
